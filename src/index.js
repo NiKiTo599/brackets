cb0546e594a06bd10e64e9d0dc779cdb83868f46
NiKiTo599 <akulichnikita0@gmail.com>
2019-03-16 23:17:30 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var testStr = str.split('');
+  for (i = 0; i < bracketsConfig.length; i++) {
+    var count = testStr.length;
+    for (j = 0; j < testStr.length; j++) {
+      if (testStr[j] == bracketsConfig[i][0] && testStr[j+1] == bracketsConfig[i][1]) {
+        testStr.splice(j,2);
+        j = -1;
+      }
+    }
+    if (testStr.length < count) {
+      i = -1;
+    }
+  }
+  return !testStr.length;
 }
